6d7934466e80dc11275f7845749fac6f655c0a3f
charint4 <rafifridhoo@gmail.com>
2021-06-09 04:17:37 +0700

    create deleteHandler for when deleting todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,17 +1,19 @@
 import React from 'react';
 
-const Todo = ({ text, id }) => {
+const Todo = ({ text, todo, todos, setTodos }) => {
+  const deleteHandler = () => {
+    setTodos(todos.filter((item) => item.id !== todo.id));
+  };
+
   return (
     <div className='todo'>
-      <li className='todo-item' key={id}>
-        {text}
-      </li>
+      <li className='todo-item'>{text}</li>
       <div className='todo'>
         <button className='complete-btn'>
           {' '}
           <i className='fas fa-check'></i>
         </button>
-        <button className='trash-btn'>
+        <button className='trash-btn' onClick={deleteHandler}>
           {' '}
           <i className='fas fa-trash'></i>
         </button>
