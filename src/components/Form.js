aff74130e1f6c30b14303f5bc783df78cb89804a
charint4 <rafifridhoo@gmail.com>
2021-06-09 04:47:25 +0700

    create filter by completed

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Form = ({ inputText, setInputText, todos, setTodos }) => {
+const Form = ({ inputText, setInputText, todos, setTodos, setStatus }) => {
   const inputTextHandler = (e) => {
     e.preventDefault();
     setInputText(e.target.value);
@@ -19,6 +19,9 @@ const Form = ({ inputText, setInputText, todos, setTodos }) => {
     setInputText('');
   };
 
+  const statusHandler = (e) => {
+    setStatus(e.target.value);
+  };
   return (
     <form>
       <input
@@ -31,7 +34,11 @@ const Form = ({ inputText, setInputText, todos, setTodos }) => {
         <i className='fas fa-plus-square'></i>
       </button>
       <div className='select'>
-        <select name='todos' id='todos' className='filter-todo'>
+        <select
+          onChange={statusHandler}
+          name='todos'
+          id='todos'
+          className='filter-todo'>
           <option value='all'>All</option>
           <option value='completed'>Completed</option>
           <option value='uncompleted'>Uncompleted</option>
